b8407f6d7dfd7af8a256051210ef9dca784fa96e
kokolihapihvi <antsa.s@hotmail.com>
2013-09-26 22:53:04 +0300

    Working perlin noise

--- src/mist2meat/javatestgame/noise/PerlinNoise.java
+++ src/mist2meat/javatestgame/noise/PerlinNoise.java
@@ -1,88 +1,136 @@
 package mist2meat.javatestgame.noise;
 
-import java.util.Random;
-
 public class PerlinNoise {
-	
-	public static float[][] generateNoise(int width, int height, int octaves, float persistance, float amplitude, float frequency) {
-		float[][] whitenoise = generateWhiteNoise(width,height);
+	public static float noise(double x, double y, int nbOctave, int frequency) {
+		int result = 0;
+		int frequence256 = frequency;
+		int sx = (int) ((x) * frequence256);
+		int sy = (int) ((y) * frequence256);
+		int octave = nbOctave;
 		
-		float[][] perlinNoise = generatePerlinNoise(whitenoise, width, height, octaves, persistance, amplitude, frequency);
-		
-		return perlinNoise;
-	}
-	
-	private static float[][] generatePerlinNoise(float[][] whiteNoise, int width, int height, int octaves, float persistance, float amplitude, float frequency) {
-		float[][] perlinNoise = new float[width][height];
-		
-		float[][][] smoothNoise = new float[octaves][width][height];
-		
-		float totalAmplitude = 0.0f;
-		
-		for(int i = 0; i < octaves; i++){
-			smoothNoise[i] = generateSmoothNoise(whiteNoise, width, height, i, frequency);
-		}
-		
-		for(int octave = octaves - 1; octave >= 0; octave--) {
-			amplitude *= persistance;
-			totalAmplitude += amplitude;
-			
-			for(int x = 0; x < width; x++) {
-				for(int y = 0; y < height; y++) {
-					perlinNoise[x][y] += smoothNoise[octave][x][y] * amplitude;
-				}
-			}
-		}
-		
-		for(int x = 0; x < width; x++){
-			for(int y = 0; y < height; y++){
-				perlinNoise[x][y] /= totalAmplitude;
-			}
-		}
-		
-		return perlinNoise;
-	}
+		while (octave != 0) {
+			int bX = sx & 0xFF;
+			int bY = sy & 0xFF;
 
-	private static float[][] generateWhiteNoise(int width, int height) {
-		Random random = new Random(1337);
-		
-		float[][] whiteNoise = new float[width][height];
-		
-		for(int x = 0; x < width; x++){
-			for(int y = 0; y < height; y++){
-				whiteNoise[x][y] = (float)random.nextDouble() % 1;
-			}
+			int sxp = sx >> 8;
+			int syp = sy >> 8;
+
+			// Compute noise for each corner of current cell
+			int Y1376312589_00 = syp * 1376312589;
+			int Y1376312589_01 = Y1376312589_00 + 1376312589;
+
+			int XY1376312589_00 = sxp + Y1376312589_00;
+			int XY1376312589_10 = XY1376312589_00 + 1;
+			int XY1376312589_01 = sxp + Y1376312589_01;
+			int XY1376312589_11 = XY1376312589_01 + 1;
+
+			int XYBASE_00 = (XY1376312589_00 << 13) ^ XY1376312589_00;
+			int XYBASE_10 = (XY1376312589_10 << 13) ^ XY1376312589_10;
+			int XYBASE_01 = (XY1376312589_01 << 13) ^ XY1376312589_01;
+			int XYBASE_11 = (XY1376312589_11 << 13) ^ XY1376312589_11;
+
+			int alt1 = (XYBASE_00 * (XYBASE_00 * XYBASE_00 * 15731 + 789221) + 1376312589);
+			int alt2 = (XYBASE_10 * (XYBASE_10 * XYBASE_10 * 15731 + 789221) + 1376312589);
+			int alt3 = (XYBASE_01 * (XYBASE_01 * XYBASE_01 * 15731 + 789221) + 1376312589);
+			int alt4 = (XYBASE_11 * (XYBASE_11 * XYBASE_11 * 15731 + 789221) + 1376312589);
+
+			/*
+			 * NOTE : on for true grandiant noise uncomment following block for
+			 * true gradiant we need to perform scalar product here, gradiant
+			 * vector are created/deducted using the above pseudo random values
+			 * (alt1...alt4) : by cutting thoses values in twice values to get
+			 * for each a fixed x,y vector gradX1= alt1&0xFF gradY1=
+			 * (alt1&0xFF00)>>8
+			 * 
+			 * the last part of the PRN (alt1&0xFF0000)>>8 is used as an offset
+			 * to correct one of the gradiant problem wich is zero on cell edge
+			 * 
+			 * source vector (sXN;sYN) for scalar product are computed using
+			 * (bX,bY)
+			 * 
+			 * each four values must be replaced by the result of the following
+			 * altN=(gradXN;gradYN) scalar (sXN;sYN)
+			 * 
+			 * all the rest of the code (interpolation+accumulation) is
+			 * identical for value & gradiant noise
+			 */
+
+			/* START BLOCK FOR TRUE GRADIANT NOISE */
+
+			int grad1X = (alt1 & 0xFF) - 128;
+			int grad1Y = ((alt1 >> 8) & 0xFF) - 128;
+			int grad2X = (alt2 & 0xFF) - 128;
+			int grad2Y = ((alt2 >> 8) & 0xFF) - 128;
+			int grad3X = (alt3 & 0xFF) - 128;
+			int grad3Y = ((alt3 >> 8) & 0xFF) - 128;
+			int grad4X = (alt4 & 0xFF) - 128;
+			int grad4Y = ((alt4 >> 8) & 0xFF) - 128;
+
+			int sX1 = bX >> 1;
+			int sY1 = bY >> 1;
+			int sX2 = 128 - sX1;
+			int sY2 = sY1;
+			int sX3 = sX1;
+			int sY3 = 128 - sY1;
+			int sX4 = 128 - sX1;
+			int sY4 = 128 - sY1;
+			alt1 = (grad1X * sX1 + grad1Y * sY1) + 16384
+					+ ((alt1 & 0xFF0000) >> 9); // to avoid seams to be 0 we use
+												// an offset
+			alt2 = (grad2X * sX2 + grad2Y * sY2) + 16384
+					+ ((alt2 & 0xFF0000) >> 9);
+			alt3 = (grad3X * sX3 + grad3Y * sY3) + 16384
+					+ ((alt3 & 0xFF0000) >> 9);
+			alt4 = (grad4X * sX4 + grad4Y * sY4) + 16384
+					+ ((alt4 & 0xFF0000) >> 9);
+
+			/* END BLOCK FOR TRUE GRADIANT NOISE */
+
+			/* START BLOCK FOR VALUE NOISE */
+			/*
+			 * alt1&=0xFFFF; alt2&=0xFFFF; alt3&=0xFFFF; alt4&=0xFFFF;
+			 */
+			/* END BLOCK FOR VALUE NOISE */
+
+			/* START BLOCK FOR LINEAR INTERPOLATION */
+			// BiLinear interpolation
+			/*
+			 * int f24=(bX*bY)>>8; int f23=bX-f24; int f14=bY-f24; int
+			 * f13=256-f14-f23-f24;
+			 * 
+			 * int val=(alt1*f13+alt2*f23+alt3*f14+alt4*f24);
+			 */
+			/* END BLOCK FOR LINEAR INTERPOLATION */
+
+			// BiCubic interpolation ( in the form alt(bX) = alt[n] - (3*bX^2 -
+			// 2*bX^3) * (alt[n] - alt[n+1]) )
+			/* START BLOCK FOR BICUBIC INTERPOLATION */
+			int bX2 = (bX * bX) >> 8;
+			int bX3 = (bX2 * bX) >> 8;
+			int _3bX2 = 3 * bX2;
+			int _2bX3 = 2 * bX3;
+			int alt12 = alt1 - (((_3bX2 - _2bX3) * (alt1 - alt2)) >> 8);
+			int alt34 = alt3 - (((_3bX2 - _2bX3) * (alt3 - alt4)) >> 8);
+
+			int bY2 = (bY * bY) >> 8;
+			int bY3 = (bY2 * bY) >> 8;
+			int _3bY2 = 3 * bY2;
+			int _2bY3 = 2 * bY3;
+			int val = alt12 - (((_3bY2 - _2bY3) * (alt12 - alt34)) >> 8);
+
+			val *= 256;
+			/* END BLOCK FOR BICUBIC INTERPOLATION */
+
+			// Accumulate in result
+			result += (val << octave);
+
+			octave--;
+			sx <<= 1;
+			sy <<= 1;
+
 		}
+		float limit = 256;
 		
-		return whiteNoise;
-	}
-	
-	private static float[][] generateSmoothNoise(float[][] whiteNoise, int width, int height, int octave, float frequency) {
-		float[][] smoothNoise = new float[width][height];
-		
-		int samplePeriod = 1 << octave;
-		
-		for(int x = 0; x < width; x++){
-			int sample_x0 = (x / samplePeriod) * samplePeriod;
-			int sample_x1 = (sample_x0 + samplePeriod) % width;
-			float horizontal_blend = (x - sample_x0) * frequency;
-			
-			for(int y = 0; y < height; y++){
-				int sample_y0 = (y / samplePeriod) * samplePeriod;
-				int sample_y1 = (sample_y0 + samplePeriod) % height;
-				float vertical_blend = (y - sample_y0) * frequency;
-				
-				float top = interpolate(whiteNoise[sample_x0][sample_y0],whiteNoise[sample_x1][sample_y0],horizontal_blend);
-				float bottom = interpolate(whiteNoise[sample_x0][sample_y1],whiteNoise[sample_x1][sample_y1],horizontal_blend);
-				
-				smoothNoise[x][y] = interpolate(top,bottom,vertical_blend);
-			}
-		}
-		
-		return smoothNoise;
-	}
-	
-	private static float interpolate(float x, float y, float alpha) {
-		return x * (1 - alpha) + alpha * y;
+		return (result >>> (16 + nbOctave + 1))/limit;
 	}
 }
--- src/mist2meat/javatestgame/states/GameStateNoiseTest.java
+++ src/mist2meat/javatestgame/states/GameStateNoiseTest.java
@@ -1,27 +1,41 @@
 package mist2meat.javatestgame.states;
 
+import mist2meat.javatestgame.input.Input;
 import mist2meat.javatestgame.noise.PerlinNoise;
 
+import org.lwjgl.input.Mouse;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 
 public class GameStateNoiseTest extends GameState {
 
-	int width,height;
-	float[][] noise;
+	private int x,y,width,height;
+	private float[][] noise;
 	
 	@Override
 	public void init() {
 		width = 800;
 		height = 600;
 		
-		int octaves = 4;
-		float persistance = 0.1f;
-		float amplitude = 1f;
-		float frequency = 0.1f;
+		x = 0;
+		y = 0;
+
+		noise = new float[width][height];
+		refreshNoise();
+	}
+	
+	private void refreshNoise() {
+		noise = new float[width][height];
 		
-		noise = PerlinNoise.generateNoise(width, height, octaves, persistance, amplitude, frequency);
+		int frequency = 4;
+		int octaves = 6;
+		
+		for(int i = 0; i < width; i++){
+			for(int j = 0; j < height; j++){
+				noise[i][j] = PerlinNoise.noise((x+i), (y+j), octaves, frequency);
+			}
+		}
 	}
 
 	@Override
@@ -29,11 +43,10 @@ public class GameStateNoiseTest extends GameState {
 		for(int x = 0; x < width; x++){
 			for(int y = 0; y < height; y++){
 				float noiseval = noise[x][y];
-				if(noiseval <= 0.995) {
-					continue;
-				}
 				Color c = Color.white;
+				
 				c.a = noiseval;
+				
 				g.setColor(c);
 				g.fillRect(x,y,1,1);
 			}
@@ -42,6 +55,10 @@ public class GameStateNoiseTest extends GameState {
 	
 	@Override
 	public void update(GameContainer container, int delta) {
-		
+		if(Input.getMouse1()){			
+			x -= Mouse.getDX();
+			y += Mouse.getDY();
+			refreshNoise();
+		}
 	}
 }
