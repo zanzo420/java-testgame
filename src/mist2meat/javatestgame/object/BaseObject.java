f3993d2d5d9670fd208d43452fcb0c0c7f02826c
kokolihapihvi <antsa.s@hotmail.com>
2013-10-16 10:22:00 +0300

    Added BaseObject

---
+++ src/mist2meat/javatestgame/object/BaseObject.java
@@ -0,0 +1,18 @@
+package mist2meat.javatestgame.object;
+
+import org.newdawn.slick.Graphics;
+
+public class BaseObject {
+
+	public void init() {
+		
+	}
+	
+	public void draw(Graphics g) {
+		
+	}
+	
+	public void update() {
+		
+	}
+}
