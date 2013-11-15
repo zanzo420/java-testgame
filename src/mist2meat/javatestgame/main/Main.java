079640561ef6e7722233b4053dd064c52927b42c
kokolihapihvi <antsa.s@hotmail.com>
2013-11-15 22:50:48 +0200

    Added screen size variables

--- src/mist2meat/javatestgame/main/Main.java
+++ src/mist2meat/javatestgame/main/Main.java
@@ -4,20 +4,27 @@ import java.io.File;
 
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Vector2f;
 
 public class Main {
 
-	/**
-	 * @param args
-	 * @throws SlickException 
-	 */
+	public static final int scrw = 800;
+	public static final int scrh = 600;
+	private static Vector2f scrCenter;
+	
 	public static void main(String[] args) throws SlickException {
 		System.setProperty("org.lwjgl.librarypath", new File("lib/native/windows").getAbsolutePath());
 		
+		scrCenter = new Vector2f(scrw/2,scrh/2);
+		
 		AppGameContainer agc = new AppGameContainer(new MainGame());
 		agc.setTargetFrameRate(60);
-		agc.setDisplayMode(800, 600, false);
+		agc.setDisplayMode(scrw, scrh, false);
 		agc.start();
+	}
+	
+	public static Vector2f getScrCenter() {
+		return scrCenter.copy();
 	}
 
 }
